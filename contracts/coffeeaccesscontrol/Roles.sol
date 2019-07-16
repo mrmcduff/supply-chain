71cdc455979c242b753ec96471113c92cc681f37
Michael McDuffee <mrmcduff@gmail.com>
2019-07-16 09:58:03 -0700

    filling out v1 of the solidity functions and formatting

--- contracts/coffeeaccesscontrol/Roles.sol
+++ contracts/coffeeaccesscontrol/Roles.sol
@@ -5,40 +5,39 @@ pragma solidity ^0.4.24;
  * @dev Library for managing addresses assigned to a Role.
  */
 library Roles {
-  struct Role {
-    mapping (address => bool) bearer;
-  }
+    struct Role {
+        mapping (address => bool) bearer;
+    }
 
-  /**
-   * @dev give an account access to this role
-   */
-  function add(Role storage role, address account) internal {
-    require(account != address(0));
-    require(!has(role, account));
+    /**
+    * @dev give an account access to this role
+    */
+    function add(Role storage role, address account) internal {
+        require(account != address(0), "Address cannot be zero");
+        require(!has(role, account), "Role is already assigned");
 
-    role.bearer[account] = true;
-  }
+        role.bearer[account] = true;
+    }
 
-  /**
-   * @dev remove an account's access to this role
-   */
-  function remove(Role storage role, address account) internal {
-    require(account != address(0));
-    require(has(role, account));
+    /**
+    * @dev remove an account's access to this role
+    */
+    function remove(Role storage role, address account) internal {
+        require(account != address(0), "Address cannot be zero");
+        require(has(role, account), "Cannot remove role that is not assigned");
 
-    role.bearer[account] = false;
-  }
+        role.bearer[account] = false;
+    }
 
-  /**
-   * @dev check if an account has this role
-   * @return bool
-   */
-  function has(Role storage role, address account)
-    internal
-    view
-    returns (bool)
-  {
-    require(account != address(0));
-    return role.bearer[account];
-  }
+    /**
+    * @dev check if an account has this role
+    * @return bool
+    */
+    function has(Role storage role, address account)
+        internal
+        view
+        returns (bool) {
+        require(account != address(0), "Address cannot be zero");
+        return role.bearer[account];
+    }
 }
